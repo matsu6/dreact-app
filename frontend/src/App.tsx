792c5469bb3e5abd95f7fecc3b46b35e2d1cf766
JunyaMatsushita <matsuwork6@gmail.com>
2022-12-10 16:33:01 +0900

    styledcomponents

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { InlineStyle } from "./components/InlineStyle"
-
+import { StyledComponents } from "./components/StyledComponents"
 function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <StyledComponents />
     </div>
   )
 }
