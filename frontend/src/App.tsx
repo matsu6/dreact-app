b971858e1e6f0f50d0e279f1e32e949fd8c0c206
JunyaMatsushita <matsuwork6@gmail.com>
2022-12-10 12:41:03 +0900

    useCallback,memo化

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import React from "react"
-import SampleAxios from "./SampleAxios"
+import React, { useState, useCallback } from "react"
+import ChildArea from "./ChildArea"
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onClickOpen = () => setIsOpen(!isOpen)
+
+  const onClickUp = () => setCount(count + 1)
+  const onChangeText = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setText(e.target.value)
+
+  const onClickClose = useCallback(() => setIsOpen(false), [setIsOpen])
   return (
     <div className="App">
-      <SampleAxios />
+      <input onChange={onChangeText} value={text} />
+      <br />
+      <br />
+      <button onClick={onClickUp}>表示</button>
+      <button onClick={onClickOpen}>切り替え</button>
+      <ChildArea isOpen={isOpen} onClickClose={onClickClose} />
     </div>
   )
 }
